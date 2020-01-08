20c0163b1817aee12473b95616d7861fb1fcd730
Jay <jason_milton@hotmail.com>
2020-01-07 21:00:35 -0500

    css minor colour change. added localstorage with JSON,added check boxes, added required enty to HTML,..

--- shopping.js
+++ shopping.js
@@ -6,8 +6,10 @@ const items = [];
 
 function handleSubmit(e) {
   e.preventDefault();
-  console.log('worked');
+  console.log('submitted');
   const name = e.currentTarget.item.value;
+  // If its empty, Dont submit it --*not needed, i added
+  // if (!name) return;              *required* to HTML
   const item = {
     name,
     id: Date.now(),
@@ -19,13 +21,29 @@ function handleSubmit(e) {
   // Clear the form
   // e.currentTarget.item.value = '';   OR
   e.target.reset(); // all target
-  displayItems();
+  // fire off a custom event that will tell anyone else who cares that the items have been updated
+  list.dispatchEvent(new CustomEvent('itemsUpdated')); // dispatch event is a method that lives on All DOM Elements
 }
 
 function displayItems() {
   console.log(items);
-  const html = items.map(item => `<li>${item.name}</li>`).join('');
+  const html = items
+    .map(
+      item => `<li class="shopping-item">
+      <input type="checkbox">
+      <span class="itemName">${item.name}</span>
+      <button aria-label="Remove ${item.name}">&times;</button> 
+  </li>`
+    )
+    .join('');
   list.innerHTML = html;
 }
 
+function mirrorToLocalStorage() {
+  console.info('Saving items to localstorage');
+  localStorage.setItem('items', JSON.stringify(items));
+}
+
 shoppingForm.addEventListener('submit', handleSubmit);
+list.addEventListener('itemsUpdated', displayItems);
+list.addEventListener('itemsUpdated', mirrorToLocalStorage);
